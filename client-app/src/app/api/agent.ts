625fc5379f43aae65369ef82737475f6cc8f199c
Martin O'Grady <martinog2@gmail.com>
2023-09-13 23:30:44 -0400

    End of section 11

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -65,8 +65,14 @@ const requests = {
 const Activities = {
     list: () => requests.get<Activity[]>('/activities'),
     details: (id: string) => requests.get<Activity>(`/activities/${id}`),
-    create: (activity: Activity) => axios.post<void>('/activities', activity),
-    update: (activity: Activity) => axios.put<void>(`/activities/${activity.id}`, activity),
+    create: (activity: Activity) => {
+        console.log("MARTIN: Actvities.create");
+        axios.post<void>('/activities', activity)
+    },
+    update: (activity: Activity) => {
+        console.log("MARTIN: Actvities.update");
+        axios.put<void>(`/activities/${activity.id}`, activity) 
+    },
     delete: (id: string) => axios.delete<void>(`/activities/${id}`)
 }
 
